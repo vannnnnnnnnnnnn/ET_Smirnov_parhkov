6dbc42ee71fabd6f7a657d1ae34db3e27159ba9f
van1 <99073835+vannnnnnnnnnnnn@users.noreply.github.com>
2022-03-10 12:16:17 +0300

    Add files via upload

---
+++ LibAtestat/Students.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibAtestat
+{
+    public class Students
+    {
+        public string test(string Grypa, string GodPostup, string FIO, int Ocenka )
+        {
+            string gr = Grypa;
+            string godpos = GodPostup;
+            string fio = FIO;
+            int ocenka = Ocenka;
+
+            
+
+
+        }
+    }
+}
